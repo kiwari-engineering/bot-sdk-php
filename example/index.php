44d390cb30e2d50521ae27a0193d04a8e31460f6
andhikayuana <andhikayuana@gmail.com>
2019-04-29 10:26:37 +0700

    update' ' '

--- example/index.php
+++ example/index.php
@@ -7,31 +7,34 @@ $dotenv->load();
 
 $bot = new \Kiwari\Kiwari(getenv('ACCESS_TOKEN'));
 $bot->enableLog(true);
-$bot->run();
+// $bot->run();
 
 $sender = $bot->getSender();
 $room = $bot->getChatRoom();
 $message = $bot->getMessage();
 
-$bot->sendText($room['qiscus_room_id'], 'halo bro, ini dari kiwari bot sdk php');
+// $bot->sendText($room['qiscus_room_id'], 'halo bro, ini dari kiwari bot sdk php');
+// $bot->sendText(829055, 'hai');
 
-// $path = '/Users/andhikayuana/Downloads/wav_wav_wavv.mp3';
-// $uploadedFile = $bot->upload($path);
-$fileUrl = 'https://d1edrlpyc25xu0.cloudfront.net/kiwari-prod/raw/upload/iaczqmgLLJ/wav_wav_wavv.mp3';
-$res = $bot->sendDocument($room['qiscus_room_id'], $fileUrl);
-$res2 = $bot->sendButton($room['qiscus_room_id'], 'Ini adalah contoh teksnya ya atau bisa jadi deskripsinya', [
-    \Kiwari\Model\Button::create()
-        ->setLabel('Google')
-        ->setUrl('https://google.com'),
-    \Kiwari\Model\Button::create()
-        ->setLabel('Facebook')
-        ->setUrl('https://facebook.com'),
-    \Kiwari\Model\Button::create()
-        ->setLabel('Twitter')
-        ->setUrl('https://twitter.com')
-]);
+$path = '/Users/andhikayuana/Downloads/wav_wav_wavv.mp3';
+$uploadedFile = $bot->upload($path);
+var_dump($uploadedFile->body->data->url);
+// $fileUrl = 'https://d1edrlpyc25xu0.cloudfront.net/kiwari-prod/raw/upload/iaczqmgLLJ/wav_wav_wavv.mp3';
+$res = $bot->sendDocument(829055, $uploadedFile->body->data->url);
+var_dump($res);
+// $res2 = $bot->sendButton($room['qiscus_room_id'], 'Ini adalah contoh teksnya ya atau bisa jadi deskripsinya', [
+//     \Kiwari\Model\Button::create()
+//         ->setLabel('Google')
+//         ->setUrl('https://google.com'),
+//     \Kiwari\Model\Button::create()
+//         ->setLabel('Facebook')
+//         ->setUrl('https://facebook.com'),
+//     \Kiwari\Model\Button::create()
+//         ->setLabel('Twitter')
+//         ->setUrl('https://twitter.com')
+// ]);
 
-var_dump($res2);
+// var_dump($res2);
 // var_dump($bot->getSender());
 // var_dump($bot->getMyAccount());
 // var_dump($bot->getChatRoom());
