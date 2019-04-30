538cea2e92f3c34b8a0113f8ea63eea632a3e968
andhikayuana <andhikayuana@gmail.com>
2019-04-30 14:12:40 +0700

    update test custom

--- tests/Model/CustomTest.php
+++ tests/Model/CustomTest.php
@@ -1,111 +1,42 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-// use Kiwari\Model\Custom;
+use Kiwari\Model\Custom;
 
 class CustomTest extends TestCase
 {
-    public function testAnu(): void 
+    public function testGivenNullTypeThenShowError(): void
     {
-        $this->assertEquals(1, 1);
+        $this->expectException(\InvalidArgumentException::class);
+
+        Custom::create()->setType(null);
     }
-    // public function testGivenNullLabelThenShowError(): void
-    // {
-    //     $this->expectException(\InvalidArgumentException::class);
 
-    //     Custom::create()
-    //         ->setLabel(null);
-    // }
+    public function testGivenNullContentThenShowError(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
 
-    // public function testGivenLabelThenGetLabelAndDefaultMethodDefaultType(): void
-    // {
-    //     $label = 'Hello World';
+        Custom::create()->setContent(null);
+    }
 
-    //     $btn = Custom::create()
-    //                 ->setLabel($label);
-        
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Custom::METHOD_GET);
-    //     $this->assertEquals($btn->getType(), Custom::TYPE_LINK);
-    //     $this->assertEquals($btn->getUrl(), null);
-    //     $this->assertEquals($btn->getPayload(), null);
-    // }
+    public function testGivenTypeAndContentThenExpectJson(): void 
+    {
+        $type = 'promo';
+        $content = [
+            'id' => 123,
+            'name' => 'work chair',
+            'price' => 500000
+        ];
+        $custom = Custom::create()
+                        ->setType($type)
+                        ->setContent($content);
 
-    // public function testGivenLabelMethodPostTypePostBackPayload(): void
-    // {
-    //     $label = 'Order Here';
-    //     $payload = [
-    //         'product' => [
-    //             'id' => 1,
-    //             'name' => 'chair',
-    //             'price' => 400000,
-    //             'qty' => 10
-    //         ],
-    //         'payment' => 'BCA'
-    //     ];
+        $this->assertEquals($custom->getType(), $type);
+        $this->assertEquals($custom->getContent(), $content);
+        $this->assertEquals(json_encode($custom), json_encode([
+            'type' => 'promo',
+            'content' => $content
+        ]));
 
-    //     $btn = Custom::create()
-    //                 ->setLabel($label)
-    //                 ->setMethod(Custom::METHOD_POST)
-    //                 ->setType(Custom::TYPE_POSTBACK)
-    //                 ->setPayload($payload);
-    //     var_dump(json_encode($btn));
-        
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Custom::METHOD_POST);
-    //     $this->assertEquals($btn->getType(), Custom::TYPE_POSTBACK);
-    //     $this->assertEquals($btn->getUrl(), null);
-    //     $this->assertEquals($btn->getPayload(), $payload);
-    // }
-
-    // public function testGivenAllPropsThenExpectJson(): void 
-    // {
-    //     $label = 'Order Now!';
-    //     $payload = [
-    //         'product' => [
-    //             'id' => 23,
-    //             'name' => 'yamaha psr 3000',
-    //             'price' => 10000000,
-    //             'qty' => 1
-    //         ],
-    //         'payment' => 'Mandiri'
-    //     ];
-
-    //     $btn = Custom::create()
-    //                 ->setLabel($label)
-    //                 ->setMethod(Custom::METHOD_POST)
-    //                 ->setType(Custom::TYPE_POSTBACK)
-    //                 ->setPayload($payload);
-
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Custom::METHOD_POST);
-    //     $this->assertEquals($btn->getType(), Custom::TYPE_POSTBACK);
-    //     $this->assertEquals($btn->getUrl(), null);
-    //     $this->assertEquals($btn->getPayload(), $payload);
-
-    //     $this->assertEquals(json_encode($btn), json_encode([
-    //         'type' => Custom::TYPE_POSTBACK,
-    //         'method' => Custom::METHOD_POST,
-    //         'label' => $label,
-    //         'payload' => $payload,
-    //         'url' => null
-    //     ]));
-    // }
-
-    // public function testGivenUrlThenShowDefaultWithLink(): void
-    // {
-    //     $label = 'Go to Facebook';
-    //     $url = 'https://www.facebook.com';
-
-    //     $btn = Custom::create()
-    //                 ->setLabel($label)
-    //                 ->setUrl($url);
-
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Custom::METHOD_GET);
-    //     $this->assertEquals($btn->getType(), Custom::TYPE_LINK);
-    //     $this->assertEquals($btn->getUrl(), $url);
-    //     $this->assertEquals($btn->getPayload(), null);
-    // }
-    
+    }
 }
