e91e1cc3717b7fedfe1d458c35ba31404bb97b18
andhikayuana <andhikayuana@gmail.com>
2019-04-30 14:37:25 +0700

    location test

--- tests/Model/LocationTest.php
+++ tests/Model/LocationTest.php
@@ -1,121 +1,73 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-// use Kiwari\Model\Location;
+use Kiwari\Model\Location;
 
 class LocationTest extends TestCase
 {
-    public function testAnu(): void 
+    public function testGivenNullNameThenError()
     {
-        $this->assertEquals(1, 1);
+        $this->expectException(\InvalidArgumentException::class);
+
+        Location::create()->setName(null);
     }
-    
-    // public function testGivenNullLabelThenShowError(): void
-    // {
-    //     $this->expectException(\InvalidArgumentException::class);
 
-    //     Location::create()
-    //         ->setLabel(null);
-    // }
+    public function testGivenNullAddressThenError()
+    {
+        $this->expectException(\InvalidArgumentException::class);
 
-    // public function testGivenLabelThenGetLabelAndDefaultMethodDefaultType(): void
-    // {
-    //     $name = "Mirota Kampus 2 Simanjuntak";
-    //     $address = "Jalan C Simanjuntak No.70 ; Terban ; Gondokusuman ; Kota Yogyakarta ; Daerah Istimewa Yogyakarta 55223" ; 
-    //     $latitude = -7.776235;
-    //     $longitude = 110.374928;
-    //     $map_url = "http://maps.google.com/?q=-7.776235 ;110.374928" ; 
-    //     $encrypted_latitude = "asgahsgtwehgayw" ; 
-    //     $encrypted_longitude = "ashjshtweyghgas";
+        Location::create()->setAddress(null);
+    }
 
-    //     $location = Location::create()
-    //         ->setName($name)
-    //         ->setAddress($address)
-    //         ->setLatitude($latitude)
-    //         ->setLongitude($longitude)
-    //         ->setMapUrl($map_url)
-    //         ->setEncryptedLatitude($encrypted_latitude)
-    //         ->setEncryptedLongitude($encrypted_longitude);
-        
-    //     var_dump($location);
-    //     $this->assertEquals($location->getName(), $name);
-    // }
+    public function testGivenNulllLatThenError()
+    {
+        $this->expectException(\InvalidArgumentException::class);
 
-    // public function testGivenLabelMethodPostTypePostBackPayload(): void
-    // {
-    //     $label = 'Order Here';
-    //     $payload = [
-    //         'product' => [
-    //             'id' => 1,
-    //             'name' => 'chair',
-    //             'price' => 400000,
-    //             'qty' => 10
-    //         ],
-    //         'payment' => 'BCA'
-    //     ];
+        Location::create()->setLatitude(null);
+    }
 
-    //     $btn = Location::create()
-    //                 ->setLabel($label)
-    //                 ->setMethod(Location::METHOD_POST)
-    //                 ->setType(Location::TYPE_POSTBACK)
-    //                 ->setPayload($payload);
-    //     var_dump(json_encode($btn));
-        
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Location::METHOD_POST);
-    //     $this->assertEquals($btn->getType(), Location::TYPE_POSTBACK);
-    //     $this->assertEquals($btn->getUrl(), null);
-    //     $this->assertEquals($btn->getPayload(), $payload);
-    // }
+    public  function testGivenNullLngThenError()
+    {
+        $this->expectException(\InvalidArgumentException::class);
 
-    // public function testGivenAllPropsThenExpectJson(): void 
-    // {
-    //     $label = 'Order Now!';
-    //     $payload = [
-    //         'product' => [
-    //             'id' => 23,
-    //             'name' => 'yamaha psr 3000',
-    //             'price' => 10000000,
-    //             'qty' => 1
-    //         ],
-    //         'payment' => 'Mandiri'
-    //     ];
+        Location::create()->setLongitude(null);
+    }
 
-    //     $btn = Location::create()
-    //                 ->setLabel($label)
-    //                 ->setMethod(Location::METHOD_POST)
-    //                 ->setType(Location::TYPE_POSTBACK)
-    //                 ->setPayload($payload);
+    public function testGivenNullMapUrlThenError()
+    {
+        $this->expectException(\InvalidArgumentException::class);
 
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Location::METHOD_POST);
-    //     $this->assertEquals($btn->getType(), Location::TYPE_POSTBACK);
-    //     $this->assertEquals($btn->getUrl(), null);
-    //     $this->assertEquals($btn->getPayload(), $payload);
+        Location::create()->setMapUrl(null);
+    }
 
-    //     $this->assertEquals(json_encode($btn), json_encode([
-    //         'type' => Location::TYPE_POSTBACK,
-    //         'method' => Location::METHOD_POST,
-    //         'label' => $label,
-    //         'payload' => $payload,
-    //         'url' => null
-    //     ]));
-    // }
+    public function testGivenAllPropsThenExpectJson()
+    {
+        $name = "Mirota Kampus 2 Simanjuntak";
+        $address = "Jalan C Simanjuntak No.70 ; Terban ; Gondokusuman ; Kota Yogyakarta ; Daerah Istimewa Yogyakarta 55223"; 
+        $latitude = -7.776235;
+        $longitude = 110.374928;
+        $mapUrl = "http://maps.google.com/?q=-7.776235 ;110.374928";
 
-    // public function testGivenUrlThenShowDefaultWithLink(): void
-    // {
-    //     $label = 'Go to Facebook';
-    //     $url = 'https://www.facebook.com';
+        $location = Location::create()
+                        ->setName($name)
+                        ->setAddress($address)
+                        ->setLatitude($latitude)
+                        ->setLongitude($longitude)
+                        ->setMapUrl($mapUrl);
 
-    //     $btn = Location::create()
-    //                 ->setLabel($label)
-    //                 ->setUrl($url);
+        $this->assertEquals($location->getName(), $name);
+        $this->assertEquals($location->getAddress(), $address);
+        $this->assertEquals($location->getLongitude(), $longitude);
+        $this->assertEquals($location->getLatitude(), $latitude);
+        $this->assertEquals($location->getMapUrl(), $mapUrl);
 
-    //     $this->assertEquals($btn->getLabel(), $label);
-    //     $this->assertEquals($btn->getMethod(), Location::METHOD_GET);
-    //     $this->assertEquals($btn->getType(), Location::TYPE_LINK);
-    //     $this->assertEquals($btn->getUrl(), $url);
-    //     $this->assertEquals($btn->getPayload(), null);
-    // }
-    
+        $this->assertEquals(json_encode($location), json_encode([
+            'name' => $name,
+            'address' => $address,
+            'latitude' => $latitude,
+            'longitude' => $longitude,
+            'map_url' => $mapUrl
+        ]));
+
+    }
 }
